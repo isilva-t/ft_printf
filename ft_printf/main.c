a2f440e52fae7d353a699f9f759f1bfe388dfad9
isilva-t <ivanrfteixeira@gmail.com>
2024-07-11 10:18:00 +0100

    style main

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -1,39 +1,65 @@
 #include "ft_printf.h"
+#include <stdio.h>
+#include <limits.h>
 
-int	main(void)
-{	
-	printf ("MY: %i\n", ft_printf("%: %: 1 %% 2 % 3 %"));
-	printf ("OR: %i\n",    printf("%: %: 1 %% 2 % 3 %"));
-printf ("  MY: %i\n", ft_printf("%c	", 'c'));
-printf ("  OR: %i\n",    printf("%c	", 'c'));
-	printf ("  MY: %i\n", ft_printf(" p %p	", (void *)321321321321321));
-	printf ("  OR: %i\n",    printf(" p %p	", (void *)321321321321321));
-printf ("  MY: %i\n", ft_printf(" p %p ", 0));
-printf ("  OR: %i\n",    printf(" p %p ", 0));
-	printf ("  MY: %i\n", ft_printf("  d %d	", 31313131));
-	printf ("  OR: %i\n",    printf("  d %d	", 31313131));
-printf ("  MY: %i\n", ft_printf("   s %s	", "string"));
-printf ("  OR: %i\n",    printf("   s %s	", "string"));
-	printf ("  MY: %i\n", ft_printf("    i %i	", -1));
-	printf ("  OR: %i\n",    printf("    i %i	", -1));
-printf ("  MY: %i\n", ft_printf("    i %i	", INT_MIN));
-printf ("  OR: %i\n",    printf("    i %i	", INT_MIN));
-	printf ("  MY: %i\n", ft_printf("    i %i	", INT_MAX));
-	printf ("  OR: %i\n",    printf("    i %i	", INT_MAX));
-printf ("  MY: %i\n", ft_printf("     u %u	", UINT_MAX));
-printf ("  OR: %i\n",    printf("     u %u	", UINT_MAX));
-	printf ("  MY: %i\n", ft_printf("     u %u	", 543487));
-	printf ("  OR: %i\n",    printf("     u %u	", 543487));
-printf ("  MY: %i\n", ft_printf("     u %u	", 0));
-printf ("  OR: %i\n",    printf("     u %u	", 0));
-	printf ("  MY: %i\n", ft_printf("      X %X	", 31));
-	printf ("  OR: %i\n",    printf("      X %X	", 31));
-printf ("  MY: %i\n", ft_printf("       x %x	", 31));
-printf ("  OR: %i\n",    printf("       x %x	", 31));
-	printf ("  MY: %i\n", ft_printf("only text, only text!	"));
-	printf ("  OR: %i\n",    printf("only text, only text!	"));
-printf ("M %i\n", ft_printf("MX %c %p %d %s\n", 'c', (void *)32132, 313, "str"));
-printf ("O %i\n", printf("MX %c %p %d %s\n", 'c', (void *)32132, 313, "str"));
-printf ("MY: %i\n", ft_printf("MIX 2%i %u %X %x %%\n", -313, 31313131, 31, 31));
-printf ("OR: %i\n", printf("MIX 2%i %u %X %x %%\n", -313, 31313131, 31, 31));
+int     main(void)
+{
+    char *str_test;        
+	str_test = NULL;
+printf ("\t    MY: %i\n", ft_printf("%"));
+printf ("\t    OR: %i\n",    printf("%%"));
+
+printf ("\t    MY: %i\n", ft_printf("%: %: 1 %% 2 % 3 %"));
+printf ("\t    OR: %i\n",    printf("%%: %%: 1 %% 2 %% 3 %%"));
+
+printf ("\t    MY: %i\n", ft_printf("%c     ", 'c'));
+printf ("\t    OR: %i\n",    printf("%c     ", 'c'));
+
+printf ("\t    MY: %i\n", ft_printf(" p %p  ", (void *)321328321721391));
+printf ("\t    OR: %i\n",    printf(" p %p  ", (void *)321328321721391));
+
+printf ("\t    MY: %i\n", ft_printf(" p %p ", (void*)0));
+printf ("\t    OR: %i\n",    printf(" p %p ", (void*)0));
+
+printf ("\t    MY: %i\n", ft_printf("  d %d ", 31313131));
+printf ("\t    OR: %i\n",    printf("  d %d ", 31313131));
+
+printf ("\t    MY: %i\n", ft_printf("   s %s        ", "string"));
+printf ("\t    OR: %i\n",    printf("   s %s        ", "string"));
+
+printf ("\t    MY: %i\n", ft_printf("    i %i       ", -1));
+printf ("\t    OR: %i\n",    printf("    i %i       ", -1));
+
+printf ("\t    MY: %i\n", ft_printf("    i %i       ", INT_MIN));
+printf ("\t    OR: %i\n",    printf("    i %i       ", INT_MIN));
+
+printf ("\t    MY: %i\n", ft_printf("    i %i       ", INT_MAX));
+printf ("\t    OR: %i\n",    printf("    i %i       ", INT_MAX));
+
+printf ("\t\t    MY: %i\n", ft_printf("     u %u      ", UINT_MAX));
+printf ("\t\t    OR: %i\n",    printf("     u %u      ", UINT_MAX));
+
+printf ("\t    MY: %i\n", ft_printf("     u %u      ", 543487));
+printf ("\t    OR: %i\n",    printf("     u %u      ", 543487));
+
+printf ("\t  MY: %i\n", ft_printf("     u %u      ", 0));
+printf ("\t  OR: %i\n",    printf("     u %u      ", 0));
+
+printf ("\t\t  MY: %i\n", ft_printf("      X %X     ", 31));
+printf ("\t\t  OR: %i\n",    printf("      X %X     ", 31));
+
+printf ("\t  MY: %i\n", ft_printf("       x %x    ", 31));
+printf ("\t  OR: %i\n",    printf("       x %x    ", 31));
+
+printf ("\t  MY: %i\n", ft_printf("only text, only text!  "));
+printf ("\t  OR: %i\n",    printf("only text, only text!  "));
+
+printf ("\t\t  MY %i\n", ft_printf("MX %c %p %d %s", 'c', (void *)32132, 313, "str"));
+printf ("\t\t  OR %i\n", printf("MX %c %p %d %s", 'c', (void *)32132, 313, "str"));
+
+printf ("\t  MY: %i\n", ft_printf("MIX 2%i %u %X %x %%\t", -313, 31313131, 31, 31));
+printf ("\t  OR: %i\n", printf("MIX 2%i %u %X %x %%\t", -313, 31313131, 31, 31));
+
+printf ("\t  MY: %i\n", ft_printf("%s", str_test));
+printf ("\t  OR: %i\n", printf("%s", str_test));
 }
